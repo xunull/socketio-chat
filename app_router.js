b2a6c107a2332c6fe6a641b19afbb787a37debfc
xunull <cuiqikun@gmail.com>
2016-06-13 23:39:45 +0800

    init

---
+++ app_router.js
@@ -0,0 +1,11 @@
+
+var express = require('express');
+
+// controllers
+var home = require('./controllers/home');
+
+var router = express.Router();
+
+router.get('/',home.index);
+
+module.exports = router;
---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+    debug:true,
+    port:15321
+};
+
+module.exports = config;
