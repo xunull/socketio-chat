a90845a937ea2b7542cc5c1234d7e800915068a6
xunull <cuiqikun@gmail.com>
2016-06-19 10:37:52 +0800

    替换 用户头像的template 为使用es6  string template

--- public/res/js/ang/useravatarinfo.component.js
+++ public/res/js/ang/useravatarinfo.component.js
@@ -1,35 +1,15 @@
 var userListScope;
 
 angular.module('chatApp').component('userList', {
-    // templateaUrl: '/public/res/js/ang/useravatar.template.html',
-    // controller: function UserListController($scope) {
-    //     // // this.users = chat.users;
-    //     // $scope.users =chat.users;
-    //     // $scope.users = [{
-    //     //   username:'1111'
-    //     // }];
-    //     // // console.log(this);
-    //     // userListScope = $scope;
-    //     //
-    //     this.users = [{
-    //       username:'afdadda'
-    //     }];
-    //
-    //
-    //     console.log('12121212121');
-    // }
-
-    template: "<div>" +
-        "<div ng-repeat='user in users'>" +
-        "<img  class='user-avatar' src='/public/res/img/avatar/avatar0.png' alt='' />" +
-        "<span>{{user.username}}</span>" +
-        "</div>" +
-        "</div>",
+    template: `<div>
+                  <div ng-repeat='user in users'>
+                      <img class="user-avatar" src="/public/res/img/avatar/avatar0.png" alt="" />
+                      <span>{{user.username}}</span>
+                  </div>
+                </div>`,
     controller: function PhoneListController($scope) {
-        // this.users = [{
-        //     username: 'sdfdsfsdfdsf'
-        // }];
 
+        // 使用scope 是因为 在外界改变了 users 的值 为了使用 $scope.$apply方法
         $scope.users = chat.users;
         userListScope = $scope;
     }
