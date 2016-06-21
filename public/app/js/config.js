a496464aae9907e018254ff7fce8b0bc2e3a5f36
xunull <cuiqikun@gmail.com>
2016-06-21 15:02:38 +0800

    修改 client 的 socketio host

--- public/app/js/config.js
+++ public/app/js/config.js
@@ -1,8 +1,6 @@
 var my_config = {
     // 通讯服务器地址
-
-    // communication_server_host: 'http://pitayasa.6655.la'
-      // communication_server_host: 'http://localhost:23234'
-    communication_server_host: 'http://localhost:3000'
+    communication_server_host: window.location.href
 };
+
 module.exports = my_config;
